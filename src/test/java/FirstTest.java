2815bdfb6670283885a07e64489b18ecd3caa786
chacha86 <taejincha0619@gmail.com>
2025-01-02 15:26:46 +0900

    work4

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -7,10 +7,16 @@ public class FirstTest {
 
     @Test
     void t1() {
-
         int rst = 1;
         assertThat(rst).isEqualTo(1);
     }
 
+    @Test
+    void t2() {
+        TestApp app = new TestApp();
+        app.run();
 
+        // aaa가 출력되는가?
+        // assertThat(result).isEqualTo("aaa");
+    }
 }
