b0496b7ebc0471f474c23fd5c6186284259776db
chacha86 <taejincha0619@gmail.com>
2025-01-03 14:13:38 +0900

    command test rg

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -24,14 +24,30 @@ public class FirstTest {
     @Test
     void t3() {
         String out = TestBot.run("");
-        assertThat(out).contains("명언앱을 종료합니다.");
+        assertThat(out)
+                .contains("명령 )")
+                .contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
+    }
+    @Test
+    @DisplayName("명령을 여러번 입력할 수 있다.")
+    void t4() {
+        String out = TestBot.run("""
+                등록
+                등록
+                종료
+                """);
+
+        // "명령 )"의 출현 횟수를 계산
+        long count = out.split("명령 \\)").length - 1;
+        // 검증
+        assertThat(count).isEqualTo(3); // 기대하는 횟수에 따라 값 수정
     }
 
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
-    void t4() {
+    void t5() {
         String out = TestBot.run("");
 
         assertThat(out)
@@ -41,7 +57,7 @@ public class FirstTest {
 
     @Test
     @DisplayName("등록 - 명언 1개 입력")
-    void t5() {
+    void t6() {
         String out = TestBot.run("""
                 등록
                 현재를 사랑하라.
