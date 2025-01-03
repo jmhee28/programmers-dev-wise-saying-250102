b91c1ebbf4584189c16b1210539900f6cad41fe3
chacha86 <taejincha0619@gmail.com>
2025-01-03 12:48:24 +0900

    t5 blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -23,7 +23,7 @@ public class FirstTest {
 
     @Test
     void t3() {
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
         assertThat(out).contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
@@ -32,7 +32,7 @@ public class FirstTest {
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
@@ -42,7 +42,11 @@ public class FirstTest {
     @Test
     @DisplayName("등록 - 명언 1개 입력")
     void t5() {
-        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
 
         assertThat(out)
                 .containsSubsequence("명언 : ", "작가 : ");
--- src/test/java/TestBot.java
+++ src/test/java/TestBot.java
@@ -7,7 +7,7 @@ public class TestBot {
     // 자바 시간에 객체는 static 지양.
 
     public static String run(String input) {
-        Scanner sc = new Scanner(input + "\n");
+        Scanner sc = new Scanner(input + "종료\n");
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
