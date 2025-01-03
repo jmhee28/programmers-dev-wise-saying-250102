a37c4014d81a2d4d5db267fb2cb59ee3c30585f1
chacha86 <taejincha0619@gmail.com>
2025-01-03 12:23:06 +0900

    test4 blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -53,9 +53,12 @@ public class FirstTest {
         TestApp app = new TestApp();
         app.run();
 
+
+        // 테스트 코드 작성이 어려움.
+        // assertJ 를 사용법을 잘 모름. -> GTP를 활용하자
+
         assertThat(out.toString())
-                .contains("== 명언 앱 ==")
-                .contains("명언앱을 종료합니다.");
+                .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
 
     }
 }
