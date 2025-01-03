e060e009b08606b15fea2394fa53b7c24dfda79c
chacha86 <taejincha0619@gmail.com>
2025-01-03 12:35:39 +0900

    test4 - blue2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -27,15 +27,7 @@ public class FirstTest {
 
     @Test
     void t3() {
-        // 테스트봇 선입력
-        Scanner sc = new Scanner("종료\n");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
+        String out = TestBot.run("종료");
         assertThat(out.toString()).contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
@@ -44,18 +36,7 @@ public class FirstTest {
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
-        // 테스트봇 선입력
-        Scanner sc = new Scanner("종료\n");
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
-
-        // 테스트 코드 작성이 어려움.
-        // assertJ 를 사용법을 잘 모름. -> GTP를 활용하자
+        String out = TestBot.run("종료");
 
         assertThat(out.toString())
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
