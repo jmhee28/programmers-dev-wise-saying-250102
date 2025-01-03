b7bf4c239312240351bbf46680fa6b93913f1ce3
chacha86 <taejincha0619@gmail.com>
2025-01-03 12:43:14 +0900

    t5 red

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -28,7 +28,7 @@ public class FirstTest {
     @Test
     void t3() {
         String out = TestBot.run("종료");
-        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+        assertThat(out).contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
     }
@@ -38,8 +38,18 @@ public class FirstTest {
     void t4() {
         String out = TestBot.run("종료");
 
-        assertThat(out.toString())
+        assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
+
+    }
+
+    @Test
+    @DisplayName("등록 - 명언 1개 입력")
+    void t5() {
+        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+
+        assertThat(out)
+                .containsSubsequence("명언 : ", "작가 : ");
 
     }
 }
