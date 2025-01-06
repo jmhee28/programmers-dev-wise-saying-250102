48a94ec316596d5d5aa49219e866f59a3d76b77a
chacha86 <taejincha0619@gmail.com>
2025-01-06 10:24:45 +0900

    command t2 r

--- src/main/java/app/global/Command.java
+++ src/main/java/app/global/Command.java
@@ -2,6 +2,14 @@ package app.global;
 
 public class Command {
 
+    public Command(String cmd) {
+
+    }
+
+    public String getActionName() {
+        return "삭제";
+    }
+
     // 쪼개기 작업
 
     // 회원
