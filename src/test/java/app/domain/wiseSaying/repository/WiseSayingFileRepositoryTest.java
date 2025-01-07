5033fb3ab369643ffc8043e337ca02911762aa3c
chacha86 <taejincha0619@gmail.com>
2025-01-07 14:52:38 +0900

    repositoryTest

---
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -0,0 +1,57 @@
+package app.domain.wiseSaying.repository;
+
+import app.domain.wiseSaying.WiseSaying;
+import app.standard.Util;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingFileRepositoryTest {
+
+    WiseSayingRepository wiseSayingRepository = new WiseSayingFileRepository();
+
+    @Test
+    @DisplayName("명언 저장")
+    void t1() {
+
+        WiseSaying wiseSaying = new WiseSaying(1,"aaa", "bbb");
+
+        wiseSayingRepository.save(wiseSaying);
+
+        String filePath = "db/wiseSaying/1.json";
+
+        boolean rst = Files.exists(Path.of(filePath));
+        assertThat(rst).isTrue();
+
+        Map<String, Object> map =  Util.Json.readAsMap(filePath);
+        WiseSaying restoredWiseSaying = WiseSaying.fromMap(map);
+
+        System.out.println(wiseSaying);
+        System.out.println(restoredWiseSaying);
+
+        assertThat(wiseSaying).isEqualTo(restoredWiseSaying);
+
+    }
+
+    @Test
+    @DisplayName("명언 삭제")
+    void t2() {
+
+        WiseSaying wiseSaying = new WiseSaying(1,"aaa", "bbb");
+
+        wiseSayingRepository.save(wiseSaying);
+        String filePath = "db/wiseSaying/1.json";
+
+        boolean delRst = wiseSayingRepository.deleteById(1);
+
+        boolean rst = Files.exists(Path.of(filePath));
+        assertThat(rst).isFalse();
+        assertThat(delRst).isTrue();
+    }
+
+}
