3ff4f4f72fcfc3bac835d186e9afec557bd33c42
chacha86 <taejincha0619@gmail.com>
2025-01-07 11:51:10 +0900

    json t6 r

--- src/test/java/app/standard/JsonTest.java
+++ src/test/java/app/standard/JsonTest.java
@@ -93,7 +93,7 @@ public class JsonTest {
     }
 
     @Test
-    @DisplayName("WiseSaying을 Map으로 변환 -> Json으로 변환")
+    @DisplayName("Map을 넘기면 Json 파일로 저장하기")
     void t5() {
 
         WiseSaying wiseSaying = new WiseSaying(1, "aaa", "bbb");
@@ -115,5 +115,20 @@ public class JsonTest {
                         }
                         """.stripIndent().trim()
                 );
+    }
+
+    @Test
+    @DisplayName("파일명을 넘기면 Map으로 읽어오기")
+    void t6() {
+
+        String filePath = "test/%d.json".formatted(1);
+
+        Map<String, Object> map = Util.Json.readAsMap(filePath);
+
+        assertThat(map)
+                .hasSize(3)
+                .containsEntry("id", 1)
+                .containsEntry("content", "aaa")
+                .containsEntry("author", "bbb");
     }
 }
